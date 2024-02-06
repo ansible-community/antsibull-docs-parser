bd7b30eb46a15ff2460c85bba5cb96570dd5c00f
Felix Fontein <felix@fontein.de>
2024-02-06 23:50:53 +0100

    Cleanup; commit the file which now contains the version.

--- noxfile.py
+++ noxfile.py
@@ -110,17 +110,6 @@ def create_vectors(session: nox.Session):
         )
 
 
-def _repl_version(session: nox.Session, new_version: str):
-    with open("pyproject.toml", "r+") as fp:
-        lines = tuple(fp)
-        fp.seek(0)
-        for line in lines:
-            if line.startswith("version = "):
-                line = f'version = "{new_version}"\n'
-            fp.write(line)
-        fp.truncate()
-
-
 def test_no_modifications(session: nox.Session) -> bool:
     modified = session.run(
         "git",
@@ -189,7 +178,13 @@ def bump(session: nox.Session):
         )
         with open(fragment_file, "w") as fp:
             print(fragment, file=fp)
-        session.run("git", "add", "pyproject.toml", fragment_file, external=True)
+        session.run(
+            "git",
+            "add",
+            "src/antsibull_docs_parser/__init__.py",
+            fragment_file,
+            external=True,
+        )
         session.run("git", "commit", "-m", f"Prepare {version}.", external=True)
     session.run("antsibull-changelog", "release")
     session.run(
@@ -198,9 +193,9 @@ def bump(session: nox.Session):
         "CHANGELOG.rst",
         "changelogs/changelog.yaml",
         "changelogs/fragments/",
-        # pyproject.toml is not committed in the last step
+        # __init__.py is not committed in the last step
         # when the release_summary fragment is created manually
-        "pyproject.toml",
+        "src/antsibull_docs_parser/__init__.py",
         external=True,
     )
     install(session, ".")  # Smoke test
@@ -226,5 +221,5 @@ def publish(session: nox.Session):
     install(session, "hatch")
     session.run("hatch", "publish", *session.posargs)
     session.run("hatch", "version", "post")
-    session.run("git", "add", "pyproject.toml", external=True)
+    session.run("git", "add", "src/antsibull_docs_parser/__init__.py", external=True)
     session.run("git", "commit", "-m", "Post-release version bump.", external=True)
